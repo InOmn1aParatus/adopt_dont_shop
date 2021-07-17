9dc4c122ec0a231e0d3e3dcd94493445286ae2a3
Matt Kragen <mattdkragen@gmail.com>
2021-07-16 17:15:23 -0700

    Add strong params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,9 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+private
+  def application_params
+    params.permit(:name, :street, :city, :state, :zip_code, :status, :description)
+  end
 end
