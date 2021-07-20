1a53cb0b1241eb24ecdbf1f39263c1a89cfbef29
Matt Kragen <mattdkragen@gmail.com>
2021-07-19 20:23:01 -0700

    Update order of dependencies

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,11 +1,11 @@
 class Application < ApplicationRecord
+  has_many :pet_applications, dependent: :destroy
+  has_many :pets, through: :pet_applications
   validates :name, presence: true
   validates :street, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true, numericality: true
-  has_many :pet_applications, dependent: :destroy
-  has_many :pets, through: :pet_applications
 
   def submittable?
     !self.pets.empty?
