26211891f1f848919808dedea7883d0ffc654d99
Matt Kragen <mattdkragen@gmail.com>
2021-07-20 12:19:38 -0700

    Create approve/reject model methods

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,12 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def approve
+    update(status: 'Approved')
+  end
+
+  def reject
+    update(status: 'Rejected')
+  end
 end
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -5,4 +5,43 @@ RSpec.describe PetApplication, type: :model do
     it { should belong_to(:pet) }
     it { should belong_to(:application) }
   end
+
+  describe 'methods' do
+    before :each do
+      @shelter1 = Shelter.create!(
+        name: "SD Humane Society",
+        city: "San Diego",
+        rank: 1,
+        foster_program: "true"
+      )
+      @app1 = Application.create!(
+        name: 'Matt Kragen',
+        street: '1234 One St',
+        city: 'Vista',
+        state: 'California',
+        zip_code: 90123,
+        status: 'Pending',
+        description: 'Definitely a dog dude'
+      )
+      @pet1 = @shelter1.pets.create!(
+        name: "Rick",
+        breed: "Dalmation",
+        adoptable: true,
+        age: 1
+      )
+      @pet_app1 = PetApplication.create(pet: @pet1, application: @app1)
+    end
+
+    it 'changes status to approve' do
+      expect(@pet_app1.status).to eq(nil)
+      @pet_app1.approve
+      expect(@pet_app1.status).to eq('Approved')
+    end
+    
+    it 'changes status to approve' do
+      expect(@pet_app1.status).to eq(nil)
+      @pet_app1.reject
+      expect(@pet_app1.status).to eq('Rejected')
+    end
+  end
 end
