8ea08676edc9b2c02e6d59a72dc9cc003cf40537
Matt Kragen <mattdkragen@gmail.com>
2021-07-21 09:56:02 -0700

    Test for independence of pet approval

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -29,16 +29,16 @@ RSpec.describe 'Admin show page' do
       adoptable: true,
       age: 2
     )
-    @pet_app = PetApplication.create(pet: @pet1, application: @app1, status: 'Pending')
+    @pet_app1 = PetApplication.create(pet: @pet1, application: @app1, status: 'Pending')
   end
 
   it 'can approve a pet for adoption' do
     visit admin_application_path(@app1)
     expect(page).to have_content(@pet1.name)
     click_button 'Approve for Adoption'
-    @pet_app.reload
+    @pet_app1.reload
 
-    expect(@pet_app.status).to eq('Approved')
+    expect(@pet_app1.status).to eq('Approved')
     expect(current_path).to eq(admin_application_path(@app1))
     expect(page).to_not have_content('Approve for Adoption')
     expect(page).to have_content('Approved for Adoption!')
@@ -48,11 +48,33 @@ RSpec.describe 'Admin show page' do
     visit admin_application_path(@app1)
     expect(page).to have_content(@pet1.name)
     click_button 'Reject Adoption Request'
-    @pet_app.reload
+    @pet_app1.reload
 
-    expect(@pet_app.status).to eq('Rejected')
+    expect(@pet_app1.status).to eq('Rejected')
     expect(current_path).to eq(admin_application_path(@app1))
     expect(page).to_not have_content('Reject Adoption Request')
     expect(page).to have_content('Adoption Request Rejected')
+  end
+
+  it 'does not hide approval/rejection buttons between applications for the same pet' do
+    app2 = Application.create!(
+      name: 'Doug Dye',
+      street: '2345 Two Ave',
+      city: 'Portland',
+      state: 'Oregon',
+      zip_code: 90234,
+      status: 'Pending',
+      description: 'Dogs love him'
+    )
+    pet_app2 = PetApplication.create(pet: @pet1, application: app2, status: 'Pending')
+    
+    visit admin_application_path(@app1)
+    expect(page).to have_content('Rick')
+    expect(find('#pets')).to have_button('Approve for Adoption')
+    click_button 'Approve for Adoption'
+
+    visit admin_application_path(app2)
+    expect(page).to have_content('Rick')
+    expect(find('#pets')).to have_button('Approve for Adoption')
   end
 end
